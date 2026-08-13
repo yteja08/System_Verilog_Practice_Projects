0340b99d8490f6cd8b272764087d18414d7ff7f6
TEJA Y <y.teja0804@gmail.com>
2026-08-13 09:57:58 +0530

    Update generator.sv

--- SV_Verification/Full_Adder/Testbench/generator.sv
+++ SV_Verification/Full_Adder/Testbench/generator.sv
@@ -14,27 +14,4 @@ class generator;
       $display("Generator:, a=%0b ,b=%0b, c=%0b", tr.a,tr.b,tr.c);
     end
   endtask
-endclassclass generator;
-
-    transaction tr;
-  mailbox #(transaction) gen2drv;
-
-    function new(mailbox #(transaction) gen2drv);
-        this.gen2drv = gen2drv;
-    endfunction
-
-    task run();
-      repeat(40) begin
-            tr = new();
-
-        if (!tr.randomize())
-          $display("Randomization Failed");
-
-            gen2drv.put(tr);
-
-            $display("Generator: a=%0d b=%0d cin=%0d",
-                     tr.a, tr.b, tr.cin);
-        end
-    endtask
-
 endclass
