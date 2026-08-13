2f7710c4f085e8f9fd4220fb4e00b47e7f0da87a
TEJA Y <y.teja0804@gmail.com>
2026-08-13 09:57:02 +0530

    Update generator.sv

--- SV_Verification/Full_Adder/Testbench/generator.sv
+++ SV_Verification/Full_Adder/Testbench/generator.sv
@@ -1,4 +1,20 @@
 class generator;
+  transaction tr;
+  mailbox gen2drv;
+  
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main();
+    repeat(15) begin
+      tr=new();
+      assert(tr.randomize());
+      gen2drv.put(tr);
+      $display("Generator:, a=%0b ,b=%0b, c=%0b", tr.a,tr.b,tr.c);
+    end
+  endtask
+endclassclass generator;
 
     transaction tr;
   mailbox #(transaction) gen2drv;
