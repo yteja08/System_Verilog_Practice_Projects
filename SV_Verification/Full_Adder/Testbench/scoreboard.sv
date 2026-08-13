47c5c192853c1a72e47a4530259872d31c12bb54
TEJA Y <y.teja0804@gmail.com>
2026-08-13 10:00:46 +0530

    Update scoreboard.sv

--- SV_Verification/Full_Adder/Testbench/scoreboard.sv
+++ SV_Verification/Full_Adder/Testbench/scoreboard.sv
@@ -1,42 +1,28 @@
 class scoreboard;
-
-    transaction tr;
-    mailbox #(transaction) mon2scb;
-
-    function new(mailbox #(transaction) mon2scb);
-        this.mon2scb = mon2scb;
-    endfunction
-
-    task run();
-
-        bit expected_sum;
-        bit expected_cout;
-
-      repeat(40) begin
-
-            mon2scb.get(tr);
-
-            expected_sum  = tr.a ^ tr.b ^ tr.cin;
-          expected_cout = ((tr.a & tr.b) |
-                           (tr.b & tr.cin) |
-                           (tr.cin & tr.a));
-
-            if ((tr.sum == expected_sum) &&
-                (tr.cout == expected_cout)) begin
-              $display("+----+");
-              $display("|PASS|");
-              $display("+----+");
-            end
-          
-            else begin
-
-              $display("+----+");
-              $display("|FAIL|");
-              $display("+----+");
-            end
-
-        end
-
-    endtask
-
+  mailbox mon2scb;
+  transaction tr2;
+  
+  bit expected_sum;
+  bit expected_carry;
+  
+  function new(mailbox mon2scb);
+    this.mon2scb=mon2scb;
+  endfunction
+  
+  task main();
+    repeat(15) begin
+      mon2scb.get(tr2);
+      $display("SCOREBOARD:, a=%0b ,b=%0b, c=%0b, sum=%0b, carry=%0b", tr2.a,tr2.b,tr2.c,tr2.sum,tr2.carry);
+      
+      expected_sum=(tr2.a^tr2.b^tr2.c);
+      expected_carry=((tr2.a&tr2.b)|(tr2.c&tr2.b)|(tr2.a&tr2.c));
+      
+      if ((expected_sum ==tr2.sum) &&  (expected_carry==tr2.carry))
+        $display("PASS");
+      else
+        $display("FAIL");
+      
+    end
+  endtask
 endclass
+    
