93b7561c048a64751870069792398cc0a053b63c
TEJA Y <y.teja0804@gmail.com>
2026-08-14 14:11:25 +0530

    Added monitor.sv

--- SV_Verification/D_FlipFlop/Testbench/monitor.sv
+++ SV_Verification/D_FlipFlop/Testbench/monitor.sv
@@ -1,56 +1,32 @@
 class monitor;
-
-    transaction tr;
-
-    mailbox #(transaction) drv2mon;
-    mailbox #(transaction) mon2scb;
-    mailbox #(bit) mon2drv_ack;
-
-    virtual full_adder_itf vitf;
-
-
-    function new(mailbox #(transaction) drv2mon,
-                 mailbox #(transaction) mon2scb,
-                 mailbox #(bit) mon2drv_ack,
-                 virtual full_adder_itf vitf);
-
-        this.drv2mon = drv2mon;
-        this.mon2scb = mon2scb;
-        this.mon2drv_ack = mon2drv_ack;
-        this.vitf = vitf;
-
-    endfunction
-
-
-    task run();
-
-        bit ack;
-
-      repeat(40) begin
-
-            // Wait for Driver
-            drv2mon.get(tr);
-
-            #1;
-
-            // Sample interface
-            tr.a    = vitf.a;
-            tr.b    = vitf.b;
-            tr.cin  = vitf.cin;
-            tr.sum  = vitf.sum;
-            tr.cout = vitf.cout;
-
-            $display("Monitor: a=%0d b=%0d cin=%0d sum=%0d cout=%0d",
-                     tr.a, tr.b, tr.cin, tr.sum, tr.cout);
-
-            // Send captured transaction to scoreboard
-            mon2scb.put(tr);
-
-            // Tell Driver it can continue
-          mon2drv_ack.put(1'b1);
-
-        end
-
-    endtask
-
+  virtual itf vitf;
+  mailbox mon2scb;
+  transaction tr2;
+  event drive_done;
+  
+  function new(virtual itf vitf, mailbox mon2scb,event drive_done);
+    this.vitf=vitf;
+    this.mon2scb=mon2scb;
+    this.drive_done=drive_done;
+  endfunction
+  
+  task main();
+    repeat(15) begin
+      
+      @drive_done;
+      
+      @(posedge vitf.clk);
+      
+    
+      #1;
+      
+      tr2=new();
+      tr2.reset=vitf.reset;
+      tr2.d=vitf.d;
+      tr2.q=vitf.q;
+      $display("MONITOR: reset=%0b, d=%0b, q=%0b",tr2.reset,tr2.d,tr2.q);
+      mon2scb.put(tr2);
+      
+    end
+  endtask
 endclass
