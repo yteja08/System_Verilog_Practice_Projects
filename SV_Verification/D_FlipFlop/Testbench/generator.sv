28427b5f55c69059d71879e2bbe9802c6b4c1f1f
TEJA Y <y.teja0804@gmail.com>
2026-08-14 14:10:11 +0530

    Added generator.sv

--- SV_Verification/D_FlipFlop/Testbench/generator.sv
+++ SV_Verification/D_FlipFlop/Testbench/generator.sv
@@ -1,24 +1,22 @@
 class generator;
 
-    transaction tr;
-  mailbox #(transaction) gen2drv;
+  transaction tr1;
+  mailbox gen2drv;
 
-    function new(mailbox #(transaction) gen2drv);
+  function new(mailbox gen2drv);
         this.gen2drv = gen2drv;
     endfunction
-
-    task run();
-      repeat(40) begin
-            tr = new();
-
-        if (!tr.randomize())
-          $display("Randomization Failed");
-
-            gen2drv.put(tr);
-
-            $display("Generator: a=%0d b=%0d cin=%0d",
-                     tr.a, tr.b, tr.cin);
-        end
-    endtask
-
+  
+  task main();
+    repeat(15) begin
+      
+      tr1=new();
+      assert(tr1.randomize());
+      gen2drv.put(tr1);
+      $display("Generator: reset=%0b ,d=%0b, q=%0b", tr1.reset,tr1.d,tr1.q);
+    end
+    $display("------------------------------------------------------------");
+    $display(">>> Displaying The Samples Happened In The Posedge Of CLK<<<");
+    $display("------------------------------------------------------------");
+  endtask
 endclass
