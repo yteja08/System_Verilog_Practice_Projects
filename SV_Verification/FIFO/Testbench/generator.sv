376846000c6c3461ed07888f8878d79f073c5188
yteja08 <y.teja0804@gmail.com>
2026-08-17 23:46:29 +0530

    Added FIFO Successfully

---
+++ SV_Verification/FIFO/Testbench/generator.sv
@@ -0,0 +1,21 @@
+class Generator;
+  Transaction tr;
+  mailbox #(Transaction) gen_dri;
+  virtual signals sig;
+  
+  function new(mailbox #(Transaction) gen_dri, virtual signals sig);
+    this.gen_dri = gen_dri;
+    this.sig = sig;
+  endfunction
+  
+  task run();
+    forever 
+      begin
+        tr = new();
+        @(posedge sig.clk);
+        void '(tr.randomize());
+        gen_dri.put(tr);
+        $display("[GEN] time=%0t", $time);
+   	  end
+  endtask
+endclass
