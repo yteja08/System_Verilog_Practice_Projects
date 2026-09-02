968356ce259b2388a303e0eeb0dc31ea838cdc89
yteja08 <y.teja0804@gmail.com>
2026-09-02 23:54:58 +0530

    Added Successfully

---
+++ SV_Verification/D_FlipFlop/Asynchronous_Reset/Testbench/driver.sv
@@ -0,0 +1,26 @@
+class driver;
+  virtual itf vitf;
+  mailbox gen2drv;
+  transaction tr;
+  
+  function new(virtual itf vitf, mailbox gen2drv);
+    this.vitf=vitf;
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task run();
+    repeat(15) begin
+      
+      gen2drv.get(tr);
+      
+      @(drv_clk);
+      
+      vitf.rst=tr.rst;
+      vitf.d=tr.d;
+      vitf.en=tr.en;
+      
+      $display("DRIVER:, rst=%0b ,d=%0b, en=%0b", vitf.rst,vitf.d,vitf.en);
+      
+    end
+  endtask
+endclass
