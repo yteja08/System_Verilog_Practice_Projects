85a30da7f804bc67ece39668ddad0c47f7cd7525
yteja08 <y.teja0804@gmail.com>
2026-09-02 23:57:01 +0530

    Added Successfully

---
+++ SV_Verification/D_FlipFlop/Asynchronous_Reset/Testbench/scoreboard.sv
@@ -0,0 +1,37 @@
+class scoreboard;
+  mailbox mon2scb;
+  transaction tr;
+  
+  bit expected_q;
+  bit expected_qbar;
+  
+  function new(mailbox mon2scb);
+    this.mon2scb=mon2scb;
+  endfunction
+  
+  task run();
+    repeat(15) begin
+      mon2scb.get(tr);
+      
+      $display("SCOREBOARD:, rst=%0b ,d=%0b, en=%0b, q=%0b, qbar=%0b", tr.rst, tr.d, tr.en, tr.q, tr.qbar);
+      
+      expected_q=(tr.rst?1'b0:(tr.en?tr.d:tr.q));
+      expected_qbar=(~expected_q);
+      
+      if (((expected_q == tr.q) && (tr.reset == 1'b0)) || ((tr.reset == 1'b1) && (expected_q == 1'b0))) begin
+        
+        $display("------------------------------------------------------------");
+        $display("PASS");
+        $display("------------------------------------------------------------");
+      end
+      
+      else begin
+        $display("------------------------------------------------------------");
+        $display("FAIL");
+        $display("------------------------------------------------------------");
+      end
+      
+    end
+  endtask
+endclass
+    
